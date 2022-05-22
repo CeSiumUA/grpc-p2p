b50abcbdc1d7bd4dbca32780e0759f7af48b7d4f
CeSiumUA <fedir.katushonok@gmail.com>
2022-05-22 23:18:15 +0300

    removed loop

--- client.go
+++ client.go
@@ -21,43 +21,38 @@ func StartP2P() {
 	}(&conn)
 	HandleError(err)
 	log.Println("client connected, local:", conn.LocalAddr(), "remote:", conn.RemoteAddr())
-	for {
-		readBytes := make([]byte, 2048)
-		readBytesCount, err := conn.Read(readBytes)
-		if err != nil {
-			fmt.Println("error reading data from connection")
-			return
-		}
-		if readBytesCount == 0 {
-			continue
-		}
-		str := string(readBytes[:readBytesCount])
-		addresses := strings.Split(str, ",")
-		log.Println("got addresses from server:", addresses)
-		addressToListen := ":" + strings.Split(conn.LocalAddr().String(), ":")[1]
-		err = conn.Close()
-		if err != nil {
-			log.Println("error closing connection")
-		}
-		listener, err := reuseport.Listen("tcp4", addressToListen)
-		HandleError(err)
-		go func(lstnr *net.Listener) {
-			for {
-				connectedPeer, err := (*lstnr).Accept()
-				if err != nil {
-					log.Println("error getting new connection", err)
-					continue
-				}
-				log.Println("new client peer connected", connectedPeer.RemoteAddr().String())
-			}
-		}(&listener)
-		fmt.Println("started listener on", listener.Addr().String())
-		peerConnection, err := reuseport.Dial("tcp", listener.Addr().String(), addresses[0])
-		if err != nil {
-			log.Println("error connecting to peer", err)
-			return
-		}
-		fmt.Println("connected to peer successfully", peerConnection.RemoteAddr().String())
-		break
+
+	readBytes := make([]byte, 2048)
+	readBytesCount, err := conn.Read(readBytes)
+	if err != nil {
+		fmt.Println("error reading data from connection")
+		return
 	}
+	str := string(readBytes[:readBytesCount])
+	addresses := strings.Split(str, ",")
+	log.Println("got addresses from server:", addresses)
+	addressToListen := ":" + strings.Split(conn.LocalAddr().String(), ":")[1]
+	err = conn.Close()
+	if err != nil {
+		log.Println("error closing connection")
+	}
+	listener, err := reuseport.Listen("tcp4", addressToListen)
+	HandleError(err)
+	go func(lstnr *net.Listener) {
+		for {
+			connectedPeer, err := (*lstnr).Accept()
+			if err != nil {
+				log.Println("error getting new connection", err)
+				continue
+			}
+			log.Println("new client peer connected", connectedPeer.RemoteAddr().String())
+		}
+	}(&listener)
+	fmt.Println("started listener on", listener.Addr().String())
+	peerConnection, err := reuseport.Dial("tcp", listener.Addr().String(), addresses[0])
+	if err != nil {
+		log.Println("error connecting to peer", err)
+		return
+	}
+	fmt.Println("connected to peer successfully", peerConnection.RemoteAddr().String())
 }
